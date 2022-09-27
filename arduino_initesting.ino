7aa2489025d1033bd01309988dcdbb1573024df0
Pkelven <pkelven@me.com>
2022-09-28 04:50:41 +0800

    12

--- arduino_initesting.ino
+++ arduino_initesting.ino
@@ -9,9 +9,5 @@ void loop() {
   // put your main code here, to run repeatedly:
   Wire.begin();
   digitalWrite(2, LOW);
-<<<<<<< HEAD
   delay(2055);
-=======
-  delay(2005);
->>>>>>> 053aac40d76203d026ffaab176fc504dfe572dcd
 }
