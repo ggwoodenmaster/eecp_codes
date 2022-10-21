8fbfb8e06add8dd45725001bbb67e0943e0c0359
Pkelven <pkelven@me.com>
2022-10-21 20:54:24 +0800

    initializing env

---
+++ keypad_UI.ino
@@ -0,0 +1,53 @@
+#include <Adafruit_GFX.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_ILI9341.h>
+#include <Adafruit_FT6206.h> // DO NOT CHANGE INCLUDE ORDER!!!
+
+#define LCD_DC 9
+#define LCD_CS 10
+//#define ICSP_SCLK 13
+//#define ICSP_MISO 12
+//#define ICSP_MOSI 11
+
+#define BLACK        0x0000  /*   0,   0,   0 */
+#define BLUE         0x001F  /*   0,   0, 255 */
+#define RED          0xF800  /* 255,   0,   0 */
+#define GREEN        0x07E0  /*   0, 255,   0 */
+#define CYAN         0x07FF  /*   0, 255, 255 */
+#define MAGENTA      0xF81F  /* 255,   0, 255 */
+#define YELLOW       0xFFE0  /* 255, 255,   0 */
+#define WHITE        0xFFFF  /* 255, 255, 255 */
+#define NAVY         0x000F  /*   0,   0, 128 */
+#define DARKGREEN    0x03E0  /*   0, 128,   0 */
+#define DARKCYAN     0x03EF  /*   0, 128, 128 */
+#define MAROON       0x7800  /* 128,   0,   0 */
+#define PURPLE       0x780F  /* 128,   0, 128 */
+#define OLIVE        0x7BE0  /* 128, 128,   0 */
+#define LIGHTGREY    0xC618  /* 192, 192, 192 */
+#define DARKGREY     0x7BEF  /* 128, 128, 128 */
+#define ORANGE       0xFD20  /* 255, 165,   0 */
+#define GREENYELLOW  0xAFE5  /* 173, 255,  47 */
+#define PINK         0xF81F  /* 255,   0, 255 */
+
+int CurrentPage = 0;
+
+Adafruit_FT6206 ts = Adafruit_FT6206();
+Adafruit_ILI9341 tft = Adafruit_ILI9341(LCD_CS, LCD_DC);
+
+void setup() {
+  Serial.begin(9600);
+  tft.begin();
+  if (!ts.begin(40)) { 
+    Serial.println("Unable to start touchscreen.");
+  } else { 
+    Serial.println("Touchscreen started.");
+  } 
+    tft.setRotation(1);
+    tft.fillScreen(CYAN);
+    delay(500);
+}
+
+void loop() {
+
+}
